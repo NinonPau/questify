8044cd5f63195d042373083f972b4fcb467f1e3c
Carlo Ackermann <carlo-ackermann@gmx.de>
2025-10-30 11:44:31 +0100

    Add level up logic comments aswell as routes for user_mood

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   get 'user_mood/home'
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :user_moods, only: [:update, :create, :edit]
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
