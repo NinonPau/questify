2aa4c4640ef5e8b639e9fd54f26bb2768c2a1e79
LucilleLacroix <lucille.lacroix74520@gmail.com>
2025-11-25 12:05:44 +0100

    add index for feloowship feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_11_25_105939) do
+ActiveRecord::Schema[7.1].define(version: 2025_11_25_110234) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_25_105939) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_ally_id"], name: "index_fellowships_on_user_ally_id"
+    t.index ["user_id", "user_ally_id"], name: "index_fellowships_on_user_id_and_user_ally_id", unique: true
     t.index ["user_id"], name: "index_fellowships_on_user_id"
   end
 
