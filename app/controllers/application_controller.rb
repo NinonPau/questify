da4986b570ffa366ec429755a8b6c3b2f7a128bc
Ninon Pau <ninon.pau@protonmail.com>
2026-01-16 16:33:12 +0100

    Add username as an authenticate parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,21 @@
 class ApplicationController < ActionController::Base
-  # Devise gem: every route will be protected by authentication
+  # Devise gem: every route will be protected by authentication through username
   before_action :authenticate_user!
-  include Pundit::Authorization
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
+  # Pundit gem: uncomment to activate authorization
+
+  # include Pundit::Authorization
 
   # Pundit: allow-list approach
-  after_action :verify_authorized, except: :index, unless: :skip_pundit?
-  after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
+  # after_action :verify_authorized, except: :index, unless: :skip_pundit?
+  # after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
 
   # Uncomment when you *really understand* Pundit!
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
