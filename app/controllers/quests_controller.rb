2eff2e4c0068e0bedf9ea722cabffe341ee18ca2
Ninon Pau <ninon.pau@protonmail.com>
2026-01-17 11:46:50 +0100

    Change back to using authentication / Create a shared dir in Views for partials / Create _quest_form as a partial / Render the form on quests#new' view

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,5 +1,5 @@
 class QuestsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_quest, only: [:new, :create]
 
   def new
