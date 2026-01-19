83257565733f59dfff98b5ea1356d1d76cb3ac72
Carlo Ackermann <carlo-ackermann@gmx.de>
2026-01-19 14:53:05 +0100

    Add further variables

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,12 +13,11 @@ user_frank = User.create!(username: "Frank", email: "frank@frank.com", password:
 puts "Created first Users: #{User.all}"
 
 puts "Creating Tasks"
-Task.create!(user_id: 1, name: "Shop groceries", description: "Get to Supermarket, don't forget Water!", daily: false, completed: false, xp: 10, date: Date.today )
-Task.create!(user_id: 2, name: "Clean the desks", description: "Get rid of dust, then use Soap", daily: false, completed: false, xp: 10, date: Date.today )
-Task.create!(user_id: 3, name: "Walk the dog", description: "Walk the woof woof through the forest.", daily: true, completed: false, xp: 50, date: Date.today )
-Task.create!(user_id: 4, name: "Do some gardening", description: "Lawn mowing and cutting plants.", daily: false, completed: false, xp: 50, date: Date.today )
-Task.create!(user_id: 5, name: "Do some cleaning", description: "Clean the living room", daily: false, completed: false, xp: 40, date: Date.today )
+Task.create!(user_id: user_renata.id, name: "Shop groceries", description: "Get to Supermarket, don't forget Water!", daily: false, completed: false, xp: 10, date: Date.today )
+Task.create!(user_id: user_ninon.id, name: "Clean the desks", description: "Get rid of dust, then use Soap", daily: false, completed: false, xp: 10, date: Date.today )
+Task.create!(user_id: user_lucille.id, name: "Walk the dog", description: "Walk the woof woof through the forest.", daily: true, completed: false, xp: 50, date: Date.today )
+Task.create!(user_id: user_carlo.id, name: "Do some gardening", description: "Lawn mowing and cutting plants.", daily: false, completed: false, xp: 50, date: Date.today )
+Task.create!(user_id: user_frank.id, name: "Do some cleaning", description: "Clean the living room", daily: false, completed: false, xp: 40, date: Date.today )
 puts "Created first Users Task: #{Task.all}."
-
 
 puts "Finished seeding with #{User.count} Users, #{UserMood.count} Usermoods and #{Task.count} Tasks."
