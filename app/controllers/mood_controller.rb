3b5fafffd8da2e3bb1228c81e716a1a318a3d2d2
Carlo Ackermann <carlo-ackermann@gmx.de>
2026-01-22 09:10:09 +0100

    Add rename fix for Moods, user_mood has been abandoned

--- app/controllers/mood_controller.rb
+++ app/controllers/mood_controller.rb
@@ -2,9 +2,9 @@ class UserMoodController < ApplicationController
   # before_action :authenticate_user! ##if implemented##
 
   def create
-    @user_mood = UserMood.new(strong_params)
-    @user_mood.user = current_user
-    if @user_mood.save
+    @mood = Mood.new(strong_params)
+    @mood.user = current_user
+    if @mood.save
       redirect_to root_path
     else
       render :home, status: :unprocessable_entity
@@ -12,21 +12,21 @@ class UserMoodController < ApplicationController
   end
 
   def edit
-    @user_mood = UserMood.find(params[:id])
+    @mood = Mood.find(params[:id])
   end
 
   def update
-    @user_mood = UserMood.find(params[:id])
-    @user_mood.update(strong_params)
-    @user_mood.xp_bonus = set_xp_bonus
-    @user_mood.save
+    @mood = Mood.find(params[:id])
+    @mood.update(strong_params)
+    @mood.xp_bonus = set_xp_bonus
+    @mood.save
     redirect_to root_path
   end
 
   private
 
   def set_xp_bonus
-    case @user_mood.mood_type
+    case @mood.mood_type
     when "Amazing" then 1.25
     when "Good" then 1.50
     when "Ok'ish" then 2.00
@@ -35,7 +35,7 @@ class UserMoodController < ApplicationController
   end
 
   def strong_params
-    params.require(:user_mood).permit(:mood_type, :xp_bonus, :date)
+    params.require(:mood).permit(:mood_type, :xp_bonus, :date)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -90,6 +90,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_04_112630) do
     t.index ["user_id"], name: "index_quests_on_user_id"
   end
 
+  create_table "user_moods", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
