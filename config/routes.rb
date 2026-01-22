65f763b69e8eacddf655ba851bcf3f4054e833ea
Carlo Ackermann <carlo-ackermann@gmx.de>
2026-01-22 09:04:43 +0100

    Add resolving of conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get 'user_mood/home'
-=======
   devise_for :users
   
->>>>>>> master
   root to: "pages#home"
   get "up" => "rails/health#show", as: :rails_health_check
   resources :user_moods, only: [:update, :create, :edit]
