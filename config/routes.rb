5d52f208e27a709aeacff215d2a50c6fc474ef00
Lucille Lacroix <lucille.lacroix74520@gmail.com>
2026-01-27 18:18:35 +0100

    Add fellowship index view

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,13 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :fellowships, only: [:index, :create, :update, :destroy]
-
+  # Fellowship routes
+  resources :fellowships, only: [:index, :create, :destroy] do
+    member do
+      patch :accept
+      patch :decline
+    end
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
