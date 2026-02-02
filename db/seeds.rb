12d2c961b06e9cd50958a2af9067a96246486534
Carlo Ackermann <carlo-ackermann@gmx.de>
2026-02-02 10:00:08 +0100

    Uncomment seeds and rename tasks to quests

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 puts "Cleaning..."
 Mood.destroy_all
-# Task.destroy_all
+Quest.destroy_all
 User.delete_all
 
 puts "Creating Users"
@@ -12,12 +12,12 @@ user_frank = User.create!(username: "Frank", email: "frank@frank.com", password:
 
 puts "Created first Users: #{User.all}"
 
-puts "Creating Tasks"
-# Task.create!(user_id: user_renata.id, name: "Shop groceries", description: "Get to Supermarket, don't forget Water!", daily: false, completed: false, xp: 10, date: Date.today )
-# Task.create!(user_id: user_ninon.id, name: "Clean the desks", description: "Get rid of dust, then use Soap", daily: false, completed: false, xp: 10, date: Date.today )
-# Task.create!(user_id: user_lucille.id, name: "Walk the dog", description: "Walk the woof woof through the forest.", daily: true, completed: false, xp: 50, date: Date.today )
-# Task.create!(user_id: user_carlo.id, name: "Do some gardening", description: "Lawn mowing and cutting plants.", daily: false, completed: false, xp: 50, date: Date.today )
-# Task.create!(user_id: user_frank.id, name: "Do some cleaning", description: "Clean the living room", daily: false, completed: false, xp: 40, date: Date.today )
-# puts "Created first Users Task: #{Task.all}."
+puts "Creating Quests"
+Quest.create!(user_id: user_renata.id, name: "Shop groceries", description: "Get to Supermarket, don't forget Water!", daily: false, completed: false, xp: 10, date: Date.today )
+Quest.create!(user_id: user_ninon.id, name: "Clean the desks", description: "Get rid of dust, then use Soap", daily: false, completed: false, xp: 10, date: Date.today )
+Quest.create!(user_id: user_lucille.id, name: "Walk the dog", description: "Walk the woof woof through the forest.", daily: true, completed: false, xp: 50, date: Date.today )
+Quest.create!(user_id: user_carlo.id, name: "Do some gardening", description: "Lawn mowing and cutting plants.", daily: false, completed: false, xp: 50, date: Date.today )
+Quest.create!(user_id: user_frank.id, name: "Do some cleaning", description: "Clean the living room", daily: false, completed: false, xp: 40, date: Date.today )
+puts "Created first Users Quest: #{Quest.all}."
 
-# puts "Finished seeding with #{User.count} Users, #{Mood.count} Usermoods and #{Task.count} Tasks."
+puts "Finished seeding with #{User.count} Users, #{Mood.count} Usermoods and #{Quest.count} Quests."
