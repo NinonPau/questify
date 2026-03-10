a1078e0c0a44e745d0809d89440883eeb4d76fe6
Carlo Ackermann <carlo-ackermann@gmx.de>
2026-03-10 09:52:43 +0100

    Add authorizations to set the mood

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -1,24 +1,27 @@
 class UserMoodController < ApplicationController
-  # before_action :authenticate_user! ##if implemented##
+  before_action :set_mood
 
   # POST /moods
   def create
-    @mood = Mood.new(strong_params)
+    authorize Mood
+    @mood = Mood.find_or_initialize_by(user: current_user)
     @mood.user = current_user
     if @mood.save
-      redirect_to root_path
+      redirect_to root_path, notice: "Mood set!"
     else
-      render :home, status: :unprocessable_entity
+      redirect_to root_path, alert: "Failed to set Mood!"
     end
   end
 
   # PATCH /moods/:id
   def edit
+    authorize @mood
     @mood = Mood.find(params[:id])
   end
 
   # PATCH /moods/:id
   def update
+    authorize @mood
     @mood = Mood.find(params[:id])
     @mood.xp_bonus = set_xp_bonus
     @mood.update(strong_params)
@@ -27,9 +30,8 @@ class UserMoodController < ApplicationController
   end
 
   private
-
-  # def strong_params
-  #   params.require(:mood).permit(:mood_type, :xp_bonus, :date)
-  # end
+  def set_mood
+    @mood = Mood.find(params[:id])
+  end
 
 end
